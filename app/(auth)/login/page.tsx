aae807a796cf55b6a2e1aaf971b81c66bc1fa5d7
Retr0-xxx <yibotang2@gmail.com>
2024-07-31 18:24:27 -0700

    init

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const title = () => {
+    return ( 
+        <div>
+            <h1>LoginPage</h1>
+        </div>
+     );
+}
+ 
+export default title;
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const title = () => {
+    return ( 
+        <div>
+            <h1>Test Page</h1>
+        </div>
+     );
+}
+ 
+export default title;
